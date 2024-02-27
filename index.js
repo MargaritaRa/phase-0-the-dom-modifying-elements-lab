42c4b9ad62187dc57399d46dc7e3009e7461bdad
Margarita Ramirez <margarita26.mr@gmail.com>
2024-02-26 20:16:39 -0500

    add file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.textContent = "This is a new header";
+
+newHeader.id = "victory";
+
+if (newHeader.id === "victory") {
+    console.log("The id of the <h1> element is set to 'victory'");
+} else {
+    console.log("The id of the <h1> element is not 'victory'");
+}
+
+newHeader.textContent = "Margarita is the champion";
+
+document.body.appendChild(newHeader);
